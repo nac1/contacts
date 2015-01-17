fc24156a6c02957737036a9ec7b5de2635d7fe0f
nac1 <ignacio.h.velasco@gmail.com>
2015-01-17 13:47:31 -0600

    p9
    asdasd

--- Practica/js/actions.js
+++ Practica/js/actions.js
@@ -17,6 +17,7 @@ ready: function(){
 //funciones de archivos
     $('#aEscribir').tap(fn.escribir);//selector para escribir
     $('#aLeer').tap(fn.leer); //selectr para leer
+    $('#nsSend').tap(fn.crear);
 
 },
 
